31a0a85a86beb3e5ca8d0c02e36f050dbe57aa5e
OscarZeng <zenghaonus@outlook.com>
2019-04-03 18:39:01 +0800

    Some updates about the probability model, minor changes.

--- Pure_Probability_Player.py
+++ Pure_Probability_Player.py
@@ -11,10 +11,13 @@ class ProbabilityPlayer(BasePokerPlayer):
 
   def declare_action(self, valid_actions, hole_card, round_state):
     
+    moneyOnTable = round_state['pot']['main']['amount']
+    moneyToCall = 20
+    moneyToRaise = 20
     wining_probability = estimate_hole_card_win_rate(300, 2, gen_cards(hole_card), gen_cards(round_state['community_card']));
     # the mathmatical expectation of Fold is 0
-    # ex_call = wining_probability*(round_state[''] + moneyToCall) - moneyToCall;
-    # ex_raise = wining_probability*(moneyOnTable + 2*moneyToRaise) - moneyToRaise;
+    ex_call = wining_probability*(moneyOnTable + moneyToCall) - moneyToCall; # math expectation for calling
+    ex_raise = wining_probability*(moneyOnTable + 2*moneyToRaise + moneyToCall) - moneyToRaise - moneyToCall; # math expectation for raising
     # if max(0, ex_call, ex_raise) == 0 
     
     has_action_raise = False
@@ -32,21 +35,29 @@ class ProbabilityPlayer(BasePokerPlayer):
             #action_call = i["action"]        
             has_action_call = True
 
-    if wining_probability > 0.5 and has_action_raise == True:        
+    if wining_probability > 0.23 and has_action_raise == True:        
         action = 'raise'
     elif has_action_call == True:
         action = 'call'
     else:
         action = 'fold'
+   # if max(ex_call, ex_raise, 0) == ex_raise and has_action_raise == True:        
+   #     action = 'raise'
+   # elif max(ex_raise, ex_call, 0) == ex_call and has_action_call == True:
+   #     action = 'call'
+   # else:
+        action = 'fold'
     print(wining_probability)
     print("\n")
-    print(self)
+    print(round_state)
     print("\n")
     print(valid_actions)
     print("\n")
-    print(hole_card)
+    print(moneyOnTable)
     print("\n")
-    print(round_state['pot']['main']['amount'])
+    print(ex_call)
+    print("\n")
+    print(ex_raise)
     return action # action returned here is sent to the poker engine
 
   
