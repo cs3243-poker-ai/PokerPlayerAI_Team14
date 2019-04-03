9af53b836f55b984d7fac7a0b66989da6b7e4e51
OscarZeng <zenghaonus@outlook.com>
2019-04-03 16:49:25 +0800

    First pure probability model version with absolute threshold(0.5). Further improvement will be made later to improve the probability model.

---
+++ Pure_Probability_Player.py
@@ -0,0 +1,69 @@
+from pypokerengine.players import BasePokerPlayer
+from time import sleep
+import pprint
+
+
+from pypokerengine.utils.card_utils import estimate_hole_card_win_rate
+from pypokerengine.utils.card_utils import gen_cards
+# This player reacts based on mathmatical expectation calculated by the current probability. 
+# The playing idea base is when the money is on the table, those money will no longer be your money.
+class ProbabilityPlayer(BasePokerPlayer):
+
+  def declare_action(self, valid_actions, hole_card, round_state):
+    
+    wining_probability = estimate_hole_card_win_rate(300, 2, gen_cards(hole_card), gen_cards(round_state['community_card']));
+    # the mathmatical expectation of Fold is 0
+    # ex_call = wining_probability*(round_state[''] + moneyToCall) - moneyToCall;
+    # ex_raise = wining_probability*(moneyOnTable + 2*moneyToRaise) - moneyToRaise;
+    # if max(0, ex_call, ex_raise) == 0 
+    
+    has_action_raise = False
+    has_action_flod = False
+    has_action_call = False
+    for i in valid_actions:
+        if i["action"] == "raise":
+            #action_raise = i["action"]
+            has_action_raise = True
+            #return action  # action returned here is sent to the poker engine
+        if i["action"] == "fold":
+            #action_fold = i["action"]
+            has_action_fold = True
+        if i["action"] == "call":
+            #action_call = i["action"]        
+            has_action_call = True
+
+    if wining_probability > 0.5 and has_action_raise == True:        
+        action = 'raise'
+    elif has_action_call == True:
+        action = 'call'
+    else:
+        action = 'fold'
+    print(wining_probability)
+    print("\n")
+    print(self)
+    print("\n")
+    print(valid_actions)
+    print("\n")
+    print(hole_card)
+    print("\n")
+    print(round_state['pot']['main']['amount'])
+    return action # action returned here is sent to the poker engine
+
+  
+  def receive_game_start_message(self, game_info):
+    pass
+
+  def receive_round_start_message(self, round_count, hole_card, seats):
+    pass
+
+  def receive_street_start_message(self, street, round_state):
+    pass
+
+  def receive_game_update_message(self, action, round_state):
+    pass
+
+  def receive_round_result_message(self, winners, hand_info, round_state):
+    pass
+
+def setup_ai():
+  return ProbabilityPlayer()
--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 from pypokerengine.api.game import setup_config, start_poker
 from randomplayer import RandomPlayer
 from raise_player import RaisedPlayer
+from Pure_Probability_Player import ProbabilityPlayer
 
 #TODO:config the config as our wish
 config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=10)
@@ -8,7 +9,7 @@ config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=10)
 
 
 config.register_player(name="f1", algorithm=RandomPlayer())
-config.register_player(name="FT2", algorithm=RaisedPlayer())
+config.register_player(name="FT2", algorithm=ProbabilityPlayer())
 
 
 game_result = start_poker(config, verbose=1)
