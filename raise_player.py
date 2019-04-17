6cc5e43b1a6366622094b5c989876300ec51c288
lancelotwillow <lancelotwillow@gmail.com>
2019-04-18 00:56:34 +0800

    resolve buggy

--- raise_player.py
+++ raise_player.py
@@ -11,15 +11,28 @@ class RaisedPlayer(BasePokerPlayer):
       self.tree.build_tree()
       self.alpha_beta = None
       self.seat = None
+      self.my_uuid = None
 
   def explore_game_tree(self, round_state, win_rate):
     # print round_state
     self.current_node = self.root
-    actions = list(map(lambda action: action['action'], round_state['action_histories']['preflop']))
+    actions = []
+    # print len(round_state['action_histories']), "***"
+    if len(round_state['action_histories']) == 1:
+        actions = round_state['action_histories']['preflop']
+    elif len(round_state['action_histories']) == 2:
+        actions = round_state['action_histories']['preflop'] + round_state['action_histories']['flop']
+    elif len(round_state['action_histories']) == 3:
+        actions = round_state['action_histories']['preflop'] + round_state['action_histories']['flop'] + \
+                  round_state['action_histories']['turn']
+    elif len(round_state['action_histories']) == 4:
+        actions = round_state['action_histories']['preflop'] + round_state['action_histories']['flop'] + \
+                  round_state['action_histories']['turn'] + round_state['action_histories']['river']
+    opponent_num = 0
     for action in actions:
-      if action == 'FOLD':
+      if action['action'] == 'FOLD':
           self.current_node = self.current_node.children[0]
-      elif action == 'CALL':
+      elif action['action'] == 'CALL':
           pos = 0
           if "Host" in self.current_node.children[1].role:
               if win_rate > 0.66:
@@ -27,28 +40,37 @@ class RaisedPlayer(BasePokerPlayer):
               elif win_rate > 0.33:
                   pos = 1
           self.current_node = self.current_node.children[1].children[pos] if "Host" in self.current_node.children[1].role else self.current_node.children[1]
-      elif action == 'RAISE':
+      elif action['action'] == 'RAISE':
+          # print actions
           self.current_node = self.current_node.children[2]
+          # if action['uuid'] != self.my_uuid:
+          #     raise_num += 1
+      if self.my_uuid != action['uuid']:
+          opponent_num += 1
+    print self.tree.my_role, win_rate
     # print self.current_node
-    final_action = self.alpha_beta.alpha_beta_search(self.current_node, win_rate)
+    final_action = self.alpha_beta.alpha_beta_search(self.current_node, win_rate, opponent_num)
     # print final_action, " !!!!!"
     return final_action
 
   def declare_action(self, valid_actions, hole_card, round_state):
 #    print "declare action!!!!"
+#     print round_state
     if len(round_state['action_histories']['preflop']) == 2:
         my_role = "SB"
+        self.my_uuid = round_state['action_histories']['preflop'][0]['uuid']
     elif len(round_state['action_histories']['preflop']) == 3:
         my_role = "BB"
+        self.my_uuid = round_state['action_histories']['preflop'][1]['uuid']
     if len(round_state['action_histories']['preflop']) <= 3:
         # print round_state
         # print my_role, "!!!"
         self.tree.set_my_role(my_role)
-        print my_role
+        # print my_role
         self.alpha_beta = AlphaBeta(self.tree)
-    fold_action, raise_action, call_action = list(map(lambda valid_action: valid_action['action'], valid_actions)) + [None] * (3 - len(valid_actions))
+    # fold_action, raise_action, call_action = list(map(lambda valid_action: valid_action['action'], valid_actions)) + [None] * (3 - len(valid_actions))
     win_rate = estimate_hole_card_win_rate(1000, 2, gen_cards(hole_card), gen_cards(round_state['community_card']))
-    print "my win rate: ", win_rate
+    # print "my win rate: ", win_rate
     # if win_rate < 0.25 and fold_action != None:
     #     return fold_action
     # # elif win_rate > 0.65 and raise_action != None:
