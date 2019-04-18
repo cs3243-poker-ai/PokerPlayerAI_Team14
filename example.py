442506eac4241d2b6b37d5fdd9f95f8febe1dd61
lancelotwillow <lancelotwillow@gmail.com>
2019-04-19 05:03:15 +0800

    add weight

--- example.py
+++ example.py
@@ -6,17 +6,21 @@ from all_raise_player import AllRaisePlayer
 from search_player import SearchPlayer
 from simulation_player import ProbabilityPlayer
 
-#TODO:config the config as our wish
-config = setup_config(max_round=100, initial_stack=10000, small_blind_amount=10)
-
 # config.register_player(name="call_player", algorithm=CallPlayer())
 # config.register_player(name="random", algorithm=RandomPlayer())
 RaisedPlayer = RaisedPlayer()
-config.register_player(name="runable", algorithm=RaisedPlayer)
-config.register_player(name="allraise", algorithm=AllRaisePlayer())
-game_result = start_poker(config, verbose=1)
-print RaisedPlayer.root
-import cPickle
-ouf = open('tree.pkl', 'w')
-self.tree = cPickle.dump()
-print game_result
+
+for i in range(20):
+	config = setup_config(max_round=200, initial_stack=10000, small_blind_amount=10)
+	config.register_player(name="runable", algorithm=RaisedPlayer)
+	config.register_player(name="allraise", algorithm=AllRaisePlayer())
+	#TODO:config the config as our wish
+	game_result = start_poker(config, verbose=1)
+	print RaisedPlayer.root
+	import cPickle
+	result = open('result' + str(i) + '.pkl', 'w')
+	cPickle.dump(RaisedPlayer.result, result)
+	print RaisedPlayer.result
+	ouf = open('tree.pkl', 'w')
+	cPickle.dump(RaisedPlayer.tree, ouf)
+	print game_result
--- pypokerengine/players.py
+++ pypokerengine/players.py
@@ -1,3 +1,5 @@
+import time
+
 class BasePokerPlayer(object):
   """Base Poker client implementation
 
@@ -45,7 +47,15 @@ class BasePokerPlayer(object):
   def respond_to_ask(self, message):
     """Called from Dealer when ask message received from RoundManager"""
     valid_actions, hole_card, round_state = self.__parse_ask_message(message)
-    return self.declare_action(valid_actions, hole_card, round_state)
+    action =  self.declare_action(valid_actions, hole_card, round_state)
+    for va in valid_actions:
+      if action == va['action'] :
+        return action
+    # print('[BUGFIX-TA] : Action Called %s, valid actions : '%action)
+    # print(valid_actions)
+    # time.sleep(10)
+    return 'fold'
+    #raise ValueError('Invalid Action Called')
 
   def receive_notification(self, message):
     """Called from Dealer when notification received from RoundManager"""
