9a02ba5631c3d6f3bd75b2e24f376001e912b0dc
lancelotwillow <lancelotwillow@gmail.com>
2019-04-19 11:06:36 +0800

    unpack resukt

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
-from gameTree import Node, Tree
-root = Node("SB", None, 10, 20, ["HA", "S5"], 2)
-tree = Tree(root)
-tree.build_tree()
-print "saving"
+# from gameTree import Node, Tree
+# root = Node("SB", None, 10, 20, ["HA", "S5"], 2)
+# tree = Tree(root)
+# tree.build_tree()
+# print "saving"
+# import cPickle
+# ouf = open('tree.pkl', 'w')
+# cPickle.dump(tree, ouf)
+# # inf = open('tree.pkl')
+# # tree = cPickle.load(inf)
+# # print "loaded"
+# # print tree.root
 import cPickle
-ouf = open('tree.pkl', 'w')
-cPickle.dump(tree, ouf)
-# inf = open('tree.pkl')
-# tree = cPickle.load(inf)
-# print "loaded"
-# print tree.root
+inf = open('result49.pkl')
+data = cPickle.load(inf)
+print data
